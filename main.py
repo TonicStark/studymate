057ac63f79d1461e87e98b4e5f988f1307f12913
francesco <francescosacco.github@gmail.com>
2023-06-17 11:42:23 +0200

    Implemented extraction of text from a file

--- main.py
+++ main.py
@@ -7,3 +7,14 @@ import itertools
 
 # Load Italian Language Model
 nlp = spacy.load('it_core_news_sm')
+
+# Get the text from a file
+def get_text(file_path: str) -> str:
+    try:
+        with open(file_path, "r", encoding="utf8") as file:
+            text = file.read()
+        return text
+    except FileNotFoundError as e:
+        raise FileNotFoundError(f"The file '{file_path}' does not exist.") from e
+    except IOError as e:
+        raise IOError(f"An error occurred while reading the file: {str(e)}") from e
