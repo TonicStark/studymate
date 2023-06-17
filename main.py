1d85c7da504ea35fa780d393380793761aaf4f5b
francesco <francescosacco.github@gmail.com>
2023-06-17 11:44:34 +0200

    Added extraction of title and paragraphs in a text

--- main.py
+++ main.py
@@ -18,3 +18,35 @@ def get_text(file_path: str) -> str:
         raise FileNotFoundError(f"The file '{file_path}' does not exist.") from e
     except IOError as e:
         raise IOError(f"An error occurred while reading the file: {str(e)}") from e
+    
+# Extract titles and paragraphs from the file content
+def extract_title_and_paragraphs(file_content: str):
+    lines = file_content.split('\n')
+    main_title = ''
+    paragraphs = {}
+
+    current_title = ''
+    current_paragraph = []
+
+    for line in lines:
+        line = line.strip()
+        if line.startswith('#'):
+            if not main_title:
+                main_title = line[1:]
+            else:
+                if current_paragraph:
+                    paragraphs[current_title] = ' '.join(current_paragraph).strip()
+                current_title = line[1:]
+                current_paragraph = []
+        elif line.startswith('##'):
+            current_title = line[2:]
+        elif current_title:
+            current_paragraph.append(line)
+
+    if current_title and current_paragraph:
+        paragraphs[current_title] = ' '.join(current_paragraph).strip()
+
+    # Remove '#' and space before each paragraph title
+    paragraphs = {title.strip('# '): content for title, content in paragraphs.items()}
+
+    return main_title, paragraphs
